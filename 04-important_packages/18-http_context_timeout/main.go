30470aebb84d40d87166ad1f0b2f5d19912d1ec1
Rafael Papastamatiou <rafaelpapas1412@gmail.com>
2023-03-29 20:40:52 -0300

    http using context with timeout

---
+++ 04-important_packages/18-http_context_timeout/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+func main() {
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, time.Duration(1)*time.Second)
+
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, "GET", "http://google.com", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	println(string(body))
+}
