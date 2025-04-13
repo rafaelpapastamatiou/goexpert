fd5b54cd578cfe30d54872e00e038f3f3af7b5a5
Rafael Papastamatiou <rafael@papastamatiou.com>
2025-04-13 19:14:09 -0300

    deploy with k8s

---
+++ 21-deploy-k8s/go.mod
@@ -0,0 +1,3 @@
+module github.com/rafaelpapastamatiou/goexpert/21-deploy-using-kubernetes-and-docker
+
+go 1.24.1
---
+++ 21-deploy-k8s/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "net/http"
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello, World!"))
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
