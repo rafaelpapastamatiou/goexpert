739113a694267127aa87b8610003fc89005a79e0
Rafael Papastamatiou <rafael@papastamatiou.com>
2025-04-13 23:37:16 -0300

    graceful shutdown in go

---
+++ 22-extras/01-graceful-shutdown/go.mod
@@ -0,0 +1,3 @@
+module github.com/rafaelpapastamatiou/goexpert/22-extras-01-graceful-shutdown
+
+go 1.24.1
---
+++ 22-extras/01-graceful-shutdown/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	server := http.Server{Addr: ":3000"}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("<h1>Hello, World!</h1>"))
+	})
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			panic(err)
+		}
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+	<-stop
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	fmt.Println("Shutting down server...")
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v", err)
+	}
+
+	fmt.Println("Server stopped gracefully")
+}
